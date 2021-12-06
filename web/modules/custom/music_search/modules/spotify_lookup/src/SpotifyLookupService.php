65f0f3ad9b1d1c60d60ae373d5c4559528202b14
Hrólfur <hrolfurgylfa@protonmail.com>
2021-12-06 11:06:10 +0000

    Started working on dependency injection and multiple modules

---
+++ web/modules/custom/music_search/modules/spotify_lookup/src/SpotifyLookupService.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Drupal\spotify_lookup;
+
+/**
+ * @file
+ * Test.
+ */
+
+/**
+ * Test.
+ */
+class SpotifyLookupService {
+
+  /**
+   * Search for a song on spotify.
+   */
+  public function search(String $text) {
+    $results = ["a", "b"];
+
+    return $results;
+  }
+
+}
